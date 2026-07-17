164405172f4a5906b35699d58d47adca13410262
fantasylee <1836064398@qq.com>
2026-07-17 14:01:43 +0800

    finish embedding

---
+++ assignment1-basics/cs336_basics/embedding.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+import torch
+import math
+from einops import einsum
+
+class Embedding(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.weight = nn.Parameter(torch.empty((num_embeddings, embedding_dim), device=device, dtype=dtype))
+        nn.init.trunc_normal_(self.weight, mean=0.0, std=1.0)
+
+    def forward(self, token_ids: torch.Tensor):
+        return self.weight[token_ids]
--- assignment1-basics/cs336_basics/linear.py
+++ assignment1-basics/cs336_basics/linear.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
-import torch.nn.functional as F
 import torch
 import math
+from einops import einsum
 
 class Linear(nn.Module):
     def __init__(self, in_features, out_features, device=None, dtype=None):
@@ -12,5 +12,5 @@ class Linear(nn.Module):
         nn.init.trunc_normal_(self.weight, mean=0.0, std=math.sqrt(2 / (in_features + out_features)))
 
     def forward(self, x):
-        return F.linear(x, self.weight)
+        return einsum(x, self.weight, "... d_in, d_out d_in -> ... d_out")
     
